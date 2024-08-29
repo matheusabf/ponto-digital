54c73001d72138c1e96934127eb48992286e4ce5
Matheus Ferreira <matheus.abentof@gmail.com>
2024-08-29 05:44:15 -0300

    Atualizando home que estava dando conflito
    Atualizando home que estava dando conflito na conexão com DB

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,35 +5,8 @@ import Relatorio from '../../components/Relatorio';
 import MeusDados from '../../components/MeusDados';
 import './index.css';
 
-
-import { initializeApp } from "firebase/app";
-import { getFirestore, getDocs, collection } from "firebase/firestore"; // pega aplicação e repassa pro getFirestore e permite conectar no banco
-
 function Home() {
-  
-const firebaseApp = initializeApp ({
-  apiKey: "AIzaSyBKYm2pJva7-Ne-EfsJna4EnSaFoZdoYws",
-  authDomain: "ponto-digital-f3f65.firebaseapp.com",
-  projectId: "ponto-digital-f3f65",
-});
 
-const [NOME, setNome] = useState("");
-const [EMAIL, setEmail] = useState("");
-const [CPF, setCpf] = useState("");
-const [LOGIN, setLogin] = useState("");
-const [SENNHA, setSenha] = useState("");
-const [USUARIOS, setUsuarios] = useState("");
-
-const db = getFirestore(firebaseApp);
-const userCollectionRef = collection(db, 'USUARIOS');
-
-useEffect(() => {
-  const getUSUARIOS = async () => {
-    const data = await getDocs(userCollectionRef)
-    setUsuarios(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-  };
-  getUSUARIOS();
-}, [])
   return (
     <div className="home-container">
       <main>
